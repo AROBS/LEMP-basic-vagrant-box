be39f35686d8744f3b468edf9e01619a147e6728
Norbert Mate <norbert.mate@arobs.com>
2014-05-29 15:46:35 +0300

    ant added

--- my_cookbooks/app/attributes/default.rb
+++ my_cookbooks/app/attributes/default.rb
@@ -2,3 +2,6 @@
 #Servername what is set for the vagrants hostmanager plugin
 default['app']['servername'] = 'test.local'
 default['app']['base_dir'] = '/vagrant_data'
+# for apache ant
+default['ant']['install_method'] = 'package'
+default['java']['jdk_version'] = '7'
