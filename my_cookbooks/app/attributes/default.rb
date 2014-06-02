b785c955220d82c6b12894d186e41ace102a3e9e
Norbert Mate <norbert.mate@arobs.com>
2014-06-02 15:09:02 +0300

    Conflict fix

--- my_cookbooks/app/attributes/default.rb
+++ my_cookbooks/app/attributes/default.rb
@@ -1,6 +1,5 @@
 
 #Servername what is set for the vagrants hostmanager plugin
-<<<<<<< HEAD
 default['app']['servername'] = ''
 default['app']['base_dir'] = '/vagrant_data'
 
@@ -8,17 +7,8 @@ default['app']['base_dir'] = '/vagrant_data'
 default['ant']['install_method'] = 'package'
 default['java']['jdk_version'] = '7'
 
-=======
-default['app']['servername'] = 'ovisto.local'
-default['app']['base_dir'] = '/vagrant_data'
-
->>>>>>> a8565b90e6b53c278157f16ad8ccf9940340216b
 #mysql config
 default['mysql']['server_root_password'] = '123qwe'
 default['mysql']['server_debian_password'] = '123qwe'
 default['mysql']['version'] = '5.5'
 default['mysql']['allow_remote_root'] = true
-<<<<<<< HEAD
-
-=======
->>>>>>> a8565b90e6b53c278157f16ad8ccf9940340216b
