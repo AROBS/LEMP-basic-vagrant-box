61ae691e7908b467c04ba518ee8e8d7949bdbb7c
mihai.mocanu <mihai.mocanu@arobs.com>
2014-12-11 12:49:28 +0200

    add graylog2 server

--- data/startup/startup.sh
+++ data/startup/startup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "Runing startup.sh..."
 
+# Remove graylog2 "RUNNING_PID" file. 
+# Sometimes when vagrant box is not properly shut down
+# graylog2 server fails to remove this file and at next
+# startup it wont start, because it finds this file.
+rm -f /usr/local/graylog2-web-interface/RUNNING_PID
+
